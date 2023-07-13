9ad6c97434be3b858886723c24b04313fad43f82
oguzhan2027 <oguzhancelik2027@gmail.com>
2023-07-13 12:14:26 +0300

    continue layer building

---
+++ PiizzaPan.DataAccessLayer/EntityFramework/EfContactDal.cs
@@ -0,0 +1,15 @@
+﻿using Piizzapan.EntityLayer.Concrete;
+using PiizzaPan.DataAccessLayer.Abstract;
+using PiizzaPan.DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiizzaPan.DataAccessLayer.EntityFramework
+{
+    public class EfContactDal:GenericRepository<Contact>,IContactDal
+    {
+    }
+}
--- PiizzaPan.DataAccessLayer/Repositories/GenericRepository.cs
+++ PiizzaPan.DataAccessLayer/Repositories/GenericRepository.cs
@@ -1,4 +1,5 @@
 ﻿using PiizzaPan.DataAccessLayer.Abstract;
+using PiizzaPan.DataAccessLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,29 +10,41 @@ namespace PiizzaPan.DataAccessLayer.Repositories
 {
     public class GenericRepository<T> : IGenericDal<T> where T : class
     {
+
         public void Delete(T t)
         {
-            throw new NotImplementedException();
+            using var context = new Context();
+            context.Remove(t);
+            context.SaveChanges();
+            
         }
 
-        public T GetBtId(int id)
+        public T GetById(int id)
         {
-            throw new NotImplementedException();
+            using var context = new Context();
+
+            return context.Set<T>().Find(id);
         }
 
         public List<T> GetList()
         {
-            throw new NotImplementedException();
+            using var context = new Context();
+            return context.Set<T>().ToList();
         }
 
         public void Insert(T t)
         {
-            throw new NotImplementedException();
+            using var context = new Context();
+            context.Add(t);
+            context.SaveChanges();
         }
 
         public void Update(T t)
         {
-            throw new NotImplementedException();
+            using var context = new Context();
+            context.Add(t);
+            context.SaveChanges();
+            
         }
     }
 }
