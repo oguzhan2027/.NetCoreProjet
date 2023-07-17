36d8e98b62183b61c45775020a37d205c21b7366
oguzhan2027 <oguzhancelik2027@gmail.com>
2023-07-17 12:21:13 +0300

    Mail

---
+++ PiizzaPan.Presentation.Layer/Models/MailRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace PiizzaPan.Presentation.Layer.Models
+{
+    public class MailRequest
+    {
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string MessageContent { get; set; }
+
+    }
+}
