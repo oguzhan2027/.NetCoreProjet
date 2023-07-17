7f134b51522ca673b48862315a1f6104032fea2a
oguzhan2027 <oguzhancelik2027@gmail.com>
2023-07-17 17:04:28 +0300

    default controller

---
+++ PiizzaPan.Presentation.Layer/ViewComponents/Default/_HeadPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace PiizzaPan.Presentation.Layer.ViewComponents.Default
+{
+    public class _HeadPartial : ViewComponent
+    {
+       public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
